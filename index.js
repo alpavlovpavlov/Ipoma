a2bc3f7093996d9240571dbf8ccc0ac897cce1e9
alpavlovpavlov <114018328+alpavlovpavlov@users.noreply.github.com>
2026-05-13 23:53:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const routs = require('./src/routs');
+const cors = require ('./src/middlewears/cors');
+const configExpress = require('./src/config/configExpress');
+
+const port = process.env.PORT;
+const connectionString = process.env.DB_URL;
+
+const app = express();
+
+configExpress(app);
+
+app.use(cors());
+app.use(routs);
+
+mongoose.connect(connectionString)
+    .then(() => {
+    console.log(`DB is connected`);
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    })
+})
+
+// Alternative approuch to protect in case of an error with DB connection... You can use also try/catch
+// mongoose.on('connect', () => console.log('DB is connected'));
+// mongoose.on('disconnect', () => console.log('DB is disconnected'));
+// mongoose.on('error', (err) => console.log(err));
