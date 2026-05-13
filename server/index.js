8c83e125757fd28b0511f9870fbdc2b93f1a62e2
alpavlovpavlov <114018328+alpavlovpavlov@users.noreply.github.com>
2026-05-13 23:54:33 +0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require ('./src/middlewears/cors');
 const configExpress = require('./src/config/configExpress');
 
 const port = process.env.PORT;
-const connectionString = `mongodb://localhost:27017/ipoma-project`;
+const connectionString = process.env.DB_URL;
 
 const app = express();
 
@@ -19,8 +19,10 @@ app.use(routs);
 mongoose.connect(connectionString)
     .then(() => {
     console.log(`DB is connected`);
-    app.listen(port, console.log(`Server is listening on http://localhost:${port}...`));
-});
+    app.listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+    })
+})
 
 // Alternative approuch to protect in case of an error with DB connection... You can use also try/catch
 // mongoose.on('connect', () => console.log('DB is connected'));
