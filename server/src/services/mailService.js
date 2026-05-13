37ea07b0e82fea2c1d4cbcc96d33194cda04e8a1
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-13 23:24:21 +0300

    fix mongodb connection

--- server/src/services/mailService.js
+++ server/src/services/mailService.js
@@ -1,12 +1,6 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: process.env.EMAIL_PORT,
-    // auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASS
-    // }
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
