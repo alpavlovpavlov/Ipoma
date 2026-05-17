891f0e98e3b08c825d5a3089162a1bc9d7aad629
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-17 13:01:10 +0300

    fix port variable

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const routs = require('./src/routs');
 const cors = require ('./src/middlewears/cors');
 const configExpress = require('./src/config/configExpress');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3030;
 const connectionString = process.env.DB_URL;
 
 const app = express();
