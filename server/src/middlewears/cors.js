7c73383265752b87e86d90dfbce632001299c49c
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-17 18:12:20 +0300

    set final cors

--- server/src/middlewears/cors.js
+++ server/src/middlewears/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ipoma.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
 
