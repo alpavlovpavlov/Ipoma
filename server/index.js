3cfdb42d8e4ad4df83dd4ac3df4e141d50fe0808
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-17 14:44:10 +0300

    add some logs in index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log('1');
+
 const routs = require('./src/routs');
+
+console.log('2');
+
 const cors = require ('./src/middlewears/cors');
+
+console.log('3');
+
 const configExpress = require('./src/config/configExpress');
+
+console.log('4');
 
 const port = process.env.PORT || 3030;
 const connectionString = process.env.DB_URL;
+
 console.log(process.env.DB_URL);
 
 const app = express();
@@ -20,7 +31,7 @@ app.use(routs);
 mongoose.connect(connectionString)
     .then(() => {
     console.log(`DB is connected`);
-    
+
     app.listen(port, () => {
         console.log(`Server is listening on port ${port}`);
     })
