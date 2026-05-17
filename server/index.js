f4fbe0e2a1335c3f3d1e96fd121fdb89635590e5
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-17 19:35:14 +0300

    next cors fix

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const routs = require('./src/routs');
-const cors = require ('./src/middlewears/cors');
+// const cors = require ('./src/middlewears/cors');
 const configExpress = require('./src/config/configExpress');
 const port = process.env.PORT || 3030;
 const connectionString = process.env.DB_URL;
 
 const app = express();
 
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://ipoma.vercel.app'
+    ],
+    credentials: true
+}));
+
 configExpress(app);
 
-app.use(cors());
+// app.use(cors());
 app.use(routs);
 
 mongoose.connect(connectionString)
