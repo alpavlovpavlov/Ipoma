dab327a2bdcfdc212fc80f5e0a65f6a375f9e7b6
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-18 22:12:32 +0300

    final mail server fix

--- server/src/services/mailService.js
+++ server/src/services/mailService.js
@@ -33,16 +33,24 @@ const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendVerificationEmail(email, link, title, content) {
-    await resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: email,
-        subject: title,//'Verify your email',
-        html: `
-            <p>${content}</p>
-            <a href="${link}">Verify Email</a>
-            <p>This link expires in 30 minutes.</p>
-        `
-    });
+    try {
+        const response = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: title,//'Verify your email',
+            html: `
+                <p>${content}</p>
+                <a href="${link}">Verify Email</a>
+                <p>This link expires in 30 minutes.</p>
+            `
+        });
+        
+        console.log('Email sent:', response);
+        
+    } catch (error) {
+        console.log('Resend error:', error);
+        throw error;
+    }
 }
 
 module.exports = {
