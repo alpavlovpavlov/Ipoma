dafccc3ff1965b3365e1cd81ce00dd61b05d4d2a
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-18 22:40:04 +0300

    vix verification link

--- server/src/services/mailService.js
+++ server/src/services/mailService.js
@@ -1,33 +1,3 @@
-// const nodemailer = require('nodemailer');
-
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//     }
-// })
-
-// async function sendEmail(email, link, title, content) {
-//     await transporter.sendMail({
-//         from: process.env.EMAIL_FROM,
-//         to: email,
-//         subject: title,
-//         html: `
-//             <p>${content}</p>
-//             <a href="${link}">Click</a>
-//             <p>This link expires in 30 minutes.</p>
-//         `,
-//         replyTo: process.env.EMAIL_REPLY_TO
-//     })
-// }
-
-// module.exports = {
-//     sendEmail
-// }
-
 const { Resend } = require('resend');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
