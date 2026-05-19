c0c4eb7acc10072fd02287829f6ed0e703d49f58
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-19 22:01:21 +0300

    change date to year in create imm

--- client/src/views/create/createImm.js
+++ client/src/views/create/createImm.js
@@ -23,7 +23,7 @@ const immTemplate = () => html`
                 </div>
                 
                 <div class="input-group">
-                    <input class="input-create" type="month" name="date" />
+                    <input class="input-create" type="number" name="date" min="1900" max="2099" />
                     <label>Enter manufactoring date</label>
                 </div>
                  
