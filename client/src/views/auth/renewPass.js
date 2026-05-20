6a4b46ea0635aada188a57b57dea44709e71aaed
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-20 18:43:21 +0300

    add versel.json file

--- client/src/views/auth/renewPass.js
+++ client/src/views/auth/renewPass.js
@@ -23,8 +23,6 @@ const renewPassTemplate = () => html`
 let token = null;
 
 export function renewPassPage(ctx) {
-    console.log('Got in');
-    
     titleChange('Renew Password Page');
     context = ctx;
     token = ctx.params.token;
