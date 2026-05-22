1f90bbef88aae819ff90934df743269fbd2ac5be
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-22 14:07:31 +0300

    add log in post upload

--- server/src/controllers/immController.js
+++ server/src/controllers/immController.js
@@ -114,6 +114,7 @@ router.delete('/imm/:immId', userStatus('imm'), async (req, res) => {
 router.post('/upload',
     uploadPicturesAndDrawings.fields([{ name: 'immDrawing', maxCount: 10 }]),
     async (req, res) => {
+        console.log(req.files.immDrawing);
         try {
         const immDrawings = req.files.immDrawing?.map(x => x.path) || [];
 
