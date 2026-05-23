13ca62ce9f3bf2b9534117e057a597aa6dc9f48b
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-23 21:58:21 +0300

    slice 1

--- client/src/views/details/immDetails.js
+++ client/src/views/details/immDetails.js
@@ -139,7 +139,7 @@ export async function immDetailsPage(ctx) {
 }
 
 function view(file) {
-    console.log(file.split('/').pop().split('-').slice(0));
+    console.log(file.split('/').pop().split('-').slice(1));
     window.open(`${file}`, "_blank");
 }
 
