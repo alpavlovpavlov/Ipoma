5edaf1c5f310afb9b9abba7e802e904bf7da208b
alpavlovpavlov <al.pavlov@yahoo.com>
2026-05-23 22:16:42 +0300

    fix file save on edit page

--- client/src/views/edit/editImm.js
+++ client/src/views/edit/editImm.js
@@ -145,7 +145,6 @@ async function onEdit(event) {
             data.immDrawing = imm.immDrawing;
         } else {
             const uploadedFiles = await saveDrawing(formData);
-            console.log(uploadedFiles);
             
             data.immDrawing = uploadedFiles.immDrawings;
         }
