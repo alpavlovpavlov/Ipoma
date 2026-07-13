bdd3c63c5db757c4672cb841a21d550fa52692ad
alpavlovpavlov <al.pavlov@yahoo.com>
2026-07-13 23:42:56 +0300

    remove 0 from sanitizer

--- client/src/util/changeElementState.js
+++ client/src/util/changeElementState.js
@@ -167,11 +167,11 @@ export function inputSanitizer() {
       }
 
       if (input.classList.contains('cav-num')) {
-        input.value = input.value.replace(/[^0-9-]/g, '');
+        input.value = input.value.replace(/[^1-9-]/g, '');
       }
 
       if (input.classList.contains('mold-cav')) {
-        input.value = input.value.replace(/[^0-9+]/g, '');
+        input.value = input.value.replace(/[^1-9+]/g, '');
       }
     })
   })
