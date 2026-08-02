3bad1a6c9ec2825e992d7b96abb5993792b481a2
alpavlovpavlov <al.pavlov@yahoo.com>
2026-08-02 21:51:43 +0300

    change data sheet id

--- client/src/views/edit/editImm.js
+++ client/src/views/edit/editImm.js
@@ -72,7 +72,7 @@ const editImmTemplate = (imm, isLoading) => html`
                                 ${imm.immDataSheet.length > 0
                                     ? html`
                                         <span class="file-name">${imm.datasheets}</span>
-                                        <img id="ds-preview" src="../../../images/pdf-icon.png" style="display:none; margin-top:10px; max-width:20px" type="application/pdf" />
+                                        <img id="dataSheet-preview" src="../../../images/pdf-icon.png" style="display:none; margin-top:10px; max-width:20px" type="application/pdf" />
                                         `
                                     : html`
                                         <span class="file-name">No file selected</span>
