fc833c674c4f63d8d0264107470ee0ef53f79906
alpavlovpavlov <al.pavlov@yahoo.com>
2026-08-05 21:42:43 +0300

    add =

--- client/src/views/details/itemDetails.js
+++ client/src/views/details/itemDetails.js
@@ -53,7 +53,7 @@ const itemDetailsTemplate = (item, isLoading, currentUser) => html`
                         <p><strong>Shape</strong> ${item.shape}</p>
                         <p><strong>Cavity numbers</strong> ${item.cavityNumbers}</p>
 
-                        ${item.type == "Container"
+                        ${item.type === "Container"
                             ? html`<p><strong>Volume</strong> ${item.volume} ml</p>`
                             : null
                         }
