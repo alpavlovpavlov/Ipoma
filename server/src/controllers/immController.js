e17c6208d1e1e691fdce722802e70be34e5ac3aa
alpavlovpavlov <al.pavlov@yahoo.com>
2026-09-16 21:52:31 +0300

    remove log

--- server/src/controllers/immController.js
+++ server/src/controllers/immController.js
@@ -58,7 +58,6 @@ router.get('/imm/:id', async (req, res) => {
 // Create IMM
 router.post('/imm', hasUser(), async (req, res) => {
     const data = req.body;
-    console.log('Hello', data);
     
     try {
         if(data) {
